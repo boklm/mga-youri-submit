f815ab98a28e2be134dd455867af3ce863999a16
Pascal Terjan <pterjan@mageia.org>
2011-02-14 20:42:47 +0000

    Add a mirror action
    git-svn-id: svn+ssh://svn.mageia.org/svn/soft/build_system/mdv-youri-submit/trunk@552 5b7c3c08-40e5-403b-9995-ace06908e4af

---
+++ lib/Youri/Submit/Post/Mirror.pm
@@ -0,0 +1,55 @@
+# $Id: Gendistrib.pm 115367 2007-01-30 09:47:04Z pixel $
+package Youri::Submit::Post::Mirror;
+
+=head1 NAME
+
+Youri::Submit::Post::Mirror - synchronizes repository to mirror
+
+=head1 DESCRIPTION
+
+Calls genhdlist2
+
+=cut
+
+use warnings;
+use strict;
+use Carp;
+use base qw/Youri::Submit::Post/;
+
+sub _init {
+    my $self   = shift;
+    my %options = (
+	destination => '',
+        @_
+    );
+
+    foreach my $var ('destination') {
+	$self->{"_$var"} = $options{$var};
+    }
+}
+
+sub run {
+    my ($self, $repository, $target, $define) = @_;
+    croak "Not a class method" unless ref $self;
+    my $root = $repository->get_install_root();
+
+    croak "Missing destination" unless $self->{'_destination'};
+
+
+    if (system("rsync -alH $root/$target/ $self->{_destination}/$target/")) {
+	$self->{_error} = "Rsync command failed ($!)";
+    }
+
+    return;
+}
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010, Mageia
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+1;
+
