1b35bb5862766e2bad583d58f615b87365a333bf
Pascal Terjan <pterjan@mageia.org>
2012-03-05 20:29:54 +0000

    Add a Youri::Submit::Action to run ulri
    git-svn-id: svn+ssh://svn.mageia.org/svn/soft/build_system/mga-youri-submit/trunk@3206 5b7c3c08-40e5-403b-9995-ace06908e4af

---
+++ lib/Youri/Submit/Action/Ulri.pm
@@ -0,0 +1,53 @@
+# $Id$
+package Youri::Submit::Action::Ulri;
+
+=head1 NAME
+
+Youri::Submit::Action::Ulri - Runs ulri on the upload host
+
+=head1 DESCRIPTION
+
+This action plugin runs ulri to try starting a build
+
+=cut
+
+use warnings;
+use strict;
+use Carp;
+use base qw/Youri::Submit::Action/;
+
+sub _init {
+    my $self   = shift;
+    my %options = (
+	uphost => '',
+	user => '',
+	ssh_key => '',
+	verbose => '',
+        @_
+    );
+    croak "undefined upload host" unless $options{uphost};
+    croak "undefined ssh key" unless $options{ssh_key};
+
+    foreach my $var ('user', 'uphost', 'ssh_key', 'verbose') {
+	$self->{"_$var"} = $options{$var};
+    }
+
+    return $self;
+}
+
+sub run {
+    my ($self, $package, $repository, $target, $define) = @_;
+    croak "Not a class method" unless ref $self;
+
+    my $cmd = "ssh -i $self->{_ssh_key} $self->{_user}\@$self->{_uphost} ulri";
+    print "Submit::Action::Ulri: doing $cmd\n" if $self->{_verbose};
+}
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2002-2012, YOURI project
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+1;
