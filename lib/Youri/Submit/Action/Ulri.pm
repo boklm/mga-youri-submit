b0750a0f3b562e8f19af6933310406355e6fc961
Pascal Terjan <pterjan@mageia.org>
2012-11-25 22:26:15 +0000

    Allow setting ulri log file
    git-svn-id: svn+ssh://svn.mageia.org/svn/soft/build_system/mga-youri-submit/trunk@6511 5b7c3c08-40e5-403b-9995-ace06908e4af

--- lib/Youri/Submit/Action/Ulri.pm
+++ lib/Youri/Submit/Action/Ulri.pm
@@ -22,13 +22,14 @@ sub _init {
 	uphost => '',
 	user => '',
 	ssh_key => '',
+	logfile => '',
 	verbose => '',
         @_
     );
     croak "undefined upload host" unless $options{uphost};
     croak "undefined ssh key" unless $options{ssh_key};
 
-    foreach my $var ('user', 'uphost', 'ssh_key', 'verbose') {
+    foreach my $var ('user', 'uphost', 'ssh_key', 'logfile', 'verbose') {
 	$self->{"_$var"} = $options{$var};
     }
 
@@ -39,7 +40,11 @@ sub run {
     my ($self, $package, $repository, $target, $define) = @_;
     croak "Not a class method" unless ref $self;
 
-    my $cmd = "ssh -i $self->{_ssh_key} $self->{_user}\@$self->{_uphost} ulri";
+    my $remotecmd = "ulri";
+    if ($self->{_logfile}) {
+	$remotecmd = "ULRI_LOG_FILE=$self->{_logfile} " . $remotecmd;
+    }
+    my $cmd = "ssh -i $self->{_ssh_key} $self->{_user}\@$self->{_uphost} $remotecmd";
     print "Submit::Action::Ulri: doing $cmd\n" if $self->{_verbose};
 }
 =head1 COPYRIGHT AND LICENSE
